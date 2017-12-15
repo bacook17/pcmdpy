6e66b6cf0ed97213d3b9981ab882ab5031258ba9
Ben Cook <bcook@cfa.harvard.edu>
2017-12-15 10:50:05 -0500

    updated pcmdpy_integrate to use argparser

--- pcmdpy/pcmd_integrate.py
+++ pcmdpy/pcmd_integrate.py
@@ -2,94 +2,106 @@
 # Ben Cook (bcook@cfa.harvard.edu)
 import numpy as np
 from pcmdpy import fit_model
-
+import argparse
 import pandas as pd
-import sys
 
 import imp
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--config",
+                        help="Name of the configuration file (.py)", type=str,
+                        required=True)
+    parser.add_argument("--data", help="Name of the PCMD data file (.dat)",
+                        type=str, required=True)
+    parser.add_argument("--results", help="Name of the results file (.csv)",
+                        type=str, required=True)
+    args = parser.parse_args()
+    
     # external config file
-    setup_file = sys.argv[1]
-    setup_mod = sys.argv[1].strip('.py').rpartition('/')[-1]
-    print('Loading Setup File: %s'%setup_file)
-    setup = imp.load_source(setup_mod, setup_file)
+    config_file = args.config
+    config_mod = config_file.strip('.py').rpartition('/')[-1]
+    print('Loading Setup File: %s' % config_file)
+    config = imp.load_source(config_mod, config_file)
 
     # external data file
-    data_file = sys.argv[2]
+    data_file = args.data
     data_pcmd = np.loadtxt(data_file, unpack=True)
     
     # where to save results
-    results_file = sys.argv[3]
+    results_file = args.results
     
-    #arguments
+    # arguments
     args = {}
     args['pcmd'] = data_pcmd
-    args['filters'] = setup.filters
-    args['im_scale'] = setup.N_scale
-    args['N_points'] = setup.N_points
+    args['filters'] = config.filters
+    args['im_scale'] = config.N_scale
+    args['N_points'] = config.N_points
     try:
-        args['N_batch'] = setup.N_batch
-    except:
+        args['N_batch'] = config.N_batch
+    except KeyError:
         pass
 
-    #optional key-word arguments (defaults are set by fit_model.nested_integrate)
-    args['pool'] = setup.pool 
-    args['max_call'] = setup.N_max
-    args['gpu'] = setup.use_gpu
-    args['fixed_seed'] = setup.fixed_seed
-    args['like_mode'] = setup.like_mode
-    args['small_prior'] = setup.small_prior
+    # optional key-word arguments
+    # (defaults are set by fit_model.nested_integrate)
+    args['pool'] = config.pool
+    args['max_call'] = config.N_max
+    args['gpu'] = config.use_gpu
+    args['fixed_seed'] = config.fixed_seed
+    args['like_mode'] = config.like_mode
+    args['small_prior'] = config.small_prior
     try:
-        args['lum_cut'] = setup.lum_cut
-    except:
+        args['lum_cut'] = config.lum_cut
+    except KeyError:
         pass
     try:
-        args['dlogz'] = setup.dlogz
-    except:
+        args['dlogz'] = config.dlogz
+    except KeyError:
         pass
     try:
-        args['use_dynesty'] = setup.use_dynesty
-    except:
+        args['use_dynesty'] = config.use_dynesty
+    except KeyError:
         args['use_dynesty'] = False
 
     try:
-        args['dynamic'] = setup.dynamic
-    except:
+        args['dynamic'] = config.dynamic
+    except KeyError:
         args['dynamic'] = False
 
     try:
-        args['prior_trans'] = setup.prior_trans
-    except:
+        args['prior_trans'] = config.prior_trans
+    except KeyError:
         args['prior_trans'] = None
 
     try:
-        args['lnprior_func'] = setup.lnprior_func
-    except:
+        args['lnprior_func'] = config.lnprior_func
+    except KeyError:
         args['lnprior_func'] = None
 
     try:
-        args['bound_method'] = setup.bound_method
-    except:
+        args['bound_method'] = config.bound_method
+    except KeyError:
         args['bound_method'] = 'multi'
         
     try:
-        args['sample_method'] = setup.sample_method
-    except:
+        args['sample_method'] = config.sample_method
+    except KeyError:
         args['sample_method'] = 'unif'
 
-    args['iso_model'] = setup.iso_model
-    args['gal_class'] = setup.model_class
-    args['verbose'] = setup.verbose
+    args['iso_model'] = config.iso_model
+    args['gal_class'] = config.model_class
+    args['verbose'] = config.verbose
 
     results_cols = ['nlive', 'niter', 'ncall', 'eff',
-                     'logl', 'logwt','logvol', 'logz',
-                     'logzerr', 'h', 'delta_logz', 'time_elapsed']
-    param_names = setup.model_class._param_names
+                    'logl', 'logwt', 'logvol', 'logz',
+                    'logzerr', 'h', 'delta_logz', 'time_elapsed']
+    param_names = config.model_class._param_names
     param_names[0] = 'logzh'
     N_params = len(param_names)
-    out_file = setup.output_file
+    out_file = config.output_file
 
     for pname in param_names:
         results_cols.append(pname)
@@ -97,7 +109,7 @@ if __name__ == "__main__":
     out_df = pd.DataFrame(columns=results_cols)
     args['out_file'] = results_file
     args['out_df'] = out_df
-    args['save_every'] = 100 #fix this later
+    args['save_every'] = 100  # fix this later
     args['param_names'] = param_names
 
     print('Running Nested Sampling')
