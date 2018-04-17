50b05e18dedf91e00bbf198b0bc3972f2c4f5a94
Ben Cook <bcook@cfa.harvard.edu>
2018-04-17 15:10:29 -0400

    added plotting to init

--- pcmdpy/__init__.py
+++ pcmdpy/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['priors', 'gpu_utils', 'utils', 'driver',
-           'fit_model', 'galaxy', 'instrument', 'isochrones']
+           'fit_model', 'galaxy', 'instrument', 'isochrones', 'plotting']
 
 from . import gpu_utils
 from . import utils
@@ -9,3 +9,4 @@ from . import galaxy
 from . import instrument
 from . import isochrones
 from . import priors
+from . import plotting
