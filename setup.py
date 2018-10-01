51ded62887400d9cac5f792478897acbb65c18f9
Ben Cook <bcook@cfa.harvard.edu>
2018-10-01 15:42:32 -0400

    blah

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class CleanCommand(Command):
 
 setup(
     name='pcmdpy',
-    version='0.3.7',
+    version='0.3.8',
     author='Ben Cook',
     author_email='bcook@cfa.harvard.edu',
     packages=['pcmdpy'],
